4e1ee2c16016eb0f94520b553a5a558d0ef4f396
Baruch Lavy <ff>
2025-11-26 17:36:13 +0200

    finish basic

--- database.py
+++ database.py
@@ -1,23 +1,36 @@
-from sqlalchemy import Column,Integer,ForeignKey,DECIMAL,String,create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column,Integer,ForeignKey,Boolean,String,create_engine,CheckConstraint
+from sqlalchemy.orm import sessionmaker,DeclarativeBase
 
 DATABASE_URL = 'sqlite:///.base.db'
 engine = create_engine(DATABASE_URL)
 Session_local = sessionmaker(autoflush=True,autocommit=False,bind=engine)
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
-class User(Base):
-    __tablename__ = "users"
+class Soldier(Base):
+    __tablename__ = "soldiers"
+    id = Column(Integer,primary_key=True)
+    privet_number = Column(Integer,index=True)
+    first_name = Column(String,index=True,nullable=False)
+    last_name = Column(String,index=True,nullable=False)
+    gender = Column(String,nullable=False,index=True)
+    city = Column(String,nullable=False,index=True)
+    distance = Column(Integer,nullable=False,index=True)
+    is_assign = Column(Boolean,nullable=True,index=True)
+
+class Dorm(Base):
+    __tablename__ = 'dorm-a'
     id = Column(Integer,primary_key=True,index=True)
-    name = Column(String,index=True,nullable=False)
-    email = Column(String,unique=True,index=True,nullable=False)
-    age = Column(Integer,nullable=False)
+    dormid = Column(Integer,nullable=False)
+    soldier_1 = Column(Integer,nullable=False,index=True,unique=True)
+    soldier_2 = Column(Integer,nullable=False,index=True,unique=True)
+    soldier_3 = Column(Integer,nullable=False,index=True,unique=True)
+    soldier_4 = Column(Integer,nullable=False,index=True,unique=True)
+    soldier_5 = Column(Integer,nullable=False,index=True,unique=True)
+    soldier_6 = Column(Integer,nullable=False,index=True,unique=True)
+    soldier_7 = Column(Integer,nullable=False,index=True,unique=True)
+    soldier_8 = Column(Integer,nullable=False,index=True,unique=True)
 
-class Class(Base):
-    __tablename__ = 'classes'
-    id = Column(Integer,primary_key=True,index=True)
-    teacher = Column(String,nullable=False,index=True)
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
